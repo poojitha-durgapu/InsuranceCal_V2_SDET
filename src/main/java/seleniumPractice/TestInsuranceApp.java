0e2d55bad22fcc48dc49ff3bc846b1667e001a42
Poojitha Pothuganti <poojitha@Poojitha-Pothuganti.local>
2020-03-02 11:21:21 -0500

    navigation commands added

--- src/main/java/seleniumPractice/TestInsuranceApp.java
+++ src/main/java/seleniumPractice/TestInsuranceApp.java
@@ -2,6 +2,7 @@ package seleniumPractice;
 
 import java.util.concurrent.TimeUnit;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
@@ -9,13 +10,30 @@ import io.github.bonigarcia.wdm.WebDriverManager;
 
 public class TestInsuranceApp {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws InterruptedException {
 		// TODO Auto-generated method stub
 		WebDriverManager.chromedriver().setup();  //setup driver executables
 		WebDriver driver = new ChromeDriver(); // launch chrome driver
 		driver.get("http://demo.automationtalks.com/");
 		driver.manage().window().maximize();
 		driver.manage().timeouts().implicitlyWait(10,TimeUnit.SECONDS);
+		
+		Thread.sleep(5000);
+		driver.navigate().refresh();
+		Thread.sleep(5000);
+		
+		driver.findElement(By.id("nav_truck")).click();
+		Thread.sleep(5000);
+		
+		driver.navigate().back();
+		Thread.sleep(5000);
+		
+		driver.navigate().forward();
+		Thread.sleep(5000);
+	
+		
+		
+		
 	}
 
 }
