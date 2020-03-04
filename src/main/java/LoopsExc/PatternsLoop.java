50712cbe96c4430b02b36321956f36e570e2fa53
Poojitha Pothuganti <poojitha@Poojitha-Pothuganti.local>
2020-03-04 13:11:25 -0500

    edited

--- src/main/java/LoopsExc/PatternsLoop.java
+++ src/main/java/LoopsExc/PatternsLoop.java
@@ -17,23 +17,23 @@ public class PatternsLoop {
 		System.out.println("##########################3");
 	      //pattern2
 		System.out.println("Pattern 2");
-	      int size = 5,odd = 1, nos = size/2; // nos =number of spaces
-	        for (int i = 1; i <= size; i++) { // for number of rows i.e n rows
-	            for (int k = nos; k >= 1; k--) { // for number of spaces i.e
-	                                                // 3,2,1,0,1,2,3 and so on
+	      int size = 5,odd = 1, nos = size/2; 
+	        for (int i = 1; i <= size; i++) { 
+	            for (int k = nos; k >= 1; k--) { 
+	                                                
 	                System.out.print(" ");
 	            }
-	            for (int j = 1; j <= odd; j++) { // for number of columns i.e
-	                                                // 1,3,5,7,5,3,1
+	            for (int j = 1; j <= odd; j++) { 
+	                                               
 	                System.out.print("*");
 	            }
 	            System.out.println();
 	            if (i < size/2+1) {
-	                odd += 2; // columns increasing till center row 
-	                nos -= 1; // spaces decreasing till center row 
+	                odd += 2;  
+	                nos -= 1;
 	            } else {
-	                odd -= 2; // columns decreasing
-	                nos += 1; // spaces increasing
+	                odd -= 2; 
+	                nos += 1;
 
 	            }
 	        }
